3137547bd9c95833b9cf03781c4f07a0ad4bf372
Rudisch, Florian <Florian.Rudisch@senacor.com>
2018-04-17 11:36:05 +0200

    * angular app init

--- user-app/src/App.tsx
+++ user-app/src/App.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
-import './App.css';
 
-const logo = require('./logo.svg');
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+        <div className="row">
+            <div className="col-sm">
+                One of three columns
+            </div>
+            <div className="col-sm">
+                One of three columns
+            </div>
+            <div className="col-sm">
+                One of three columns
+            </div>
+        </div>
     );
   }
 }
