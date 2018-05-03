642e80169cbd1cd12e53f86cdf0261830307ffad
Rudisch, Florian <Florian.Rudisch@senacor.com>
2018-05-03 22:26:37 +0200

    * event app ++

--- event-app/src/app/app.module.ts
+++ event-app/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { ListComponent } from './list/list.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule
